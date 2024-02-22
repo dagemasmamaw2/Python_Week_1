d694b04baf65fe3591897e0f90c05e76b69e593c
Dagem Asmamaw <dagemasmamaw2@gmail.com>
2024-02-22 04:02:15 +0300

    adding python code

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input('Pleas enter Your Name: ')
+age = input('How old are you: ')
+location = input('From where are you: ')
+print('\nHello {}, you are {} years old and live in {}'.format(name,age,location))
